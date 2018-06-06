2d39bf56fee460f6c4ebfb5cd3c86cc87e3bd049
Tushar Varshney <juniorvarshney19@gmail.com>
2018-06-06 21:20:35 +0530

    This is not actually hello world

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+def Merger(firstname,lastname,middlename):
+    print (firstname + " " + middlename + " " + lastname)
+
+firstname ="Tushar"
+lastname = "Varshney"
+middlename = input("What is your middle name?")
+
+Merger(firstname,lastname,middlename)
