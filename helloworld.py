f49b7c7897dd1cf63f204e0c305bd6f9ab3e749f
Prakhar <32808404+prakhar2109@users.noreply.github.com>
2018-10-09 23:05:09 +0530

    Update helloworld.py
    changes in hello world

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,7 @@
 def Merger(firstname,lastname):
     print (firstname + " " + lastname)
-
 firstname =""
 lastname = ""
 firstname = input("What is your first name?")
 lastname = input("What is your last name?")
-
-
 Merger(firstname,lastname)
